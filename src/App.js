a3b06bb9d935c34d3c47d8a4c4d93ba818efa16f
Arturo G. Bruno <arturogbruno@gmail.com>
2020-03-31 12:43:59 +0200

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BoxesContainer from './components/BoxesContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BoxesContainer />
+    </div>
+  );
+}
+
+export default App;
